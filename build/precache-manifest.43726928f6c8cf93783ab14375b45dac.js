263c5f35c2481693d0dc03527e88c1413ae824e1
dj <aloanscripting@gmail.com>
2022-04-05 20:51:51 -0400

    pushing

---
+++ build/precache-manifest.43726928f6c8cf93783ab14375b45dac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b427f4ad3d40693f6770",
+    "url": "/robofriends/static/css/main.77ece5f7.chunk.css"
+  },
+  {
+    "revision": "b427f4ad3d40693f6770",
+    "url": "/robofriends/static/js/main.b427f4ad.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "682ff8874c589b94119b",
+    "url": "/robofriends/static/css/2.ebe1142f.chunk.css"
+  },
+  {
+    "revision": "682ff8874c589b94119b",
+    "url": "/robofriends/static/js/2.682ff887.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "3ca7dee2a3c1ded9f9116ef8595157cf",
+    "url": "/robofriends/index.html"
+  }
+];
